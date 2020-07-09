acd38f6ab1fb24167f8240f1d670fd34a8d04421
yeldaaa <addleegoh@gmail.com>
2020-07-09 13:46:31 +0800

    -

--- Plot_1.R
+++ Plot_1.R
@@ -1,3 +1,4 @@
+# Loading the data into R
 power_con <- read.table("household_power_consumption.txt", skip = 1, sep =";")
 names(power_con)<- c("Date", "Time", "Global_Active_Power", "Global_Reactive_Power", "Voltage", "Global_Intensity", "Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3")
 Subset_power_con <- subset(power_con, power_con$Date =="1/2/2007" | power_con$Date =="2/2/2007")
